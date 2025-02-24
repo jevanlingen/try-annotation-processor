6d12048363fe65821cfb07392ad6be9f187aff7e
lingenj <jacob.van.lingen@moderne.io>
2025-02-24 21:21:20 +0100

    Add `instance` to be more in line with MapStruct

--- src/main/java/org/jdriven/Starter.java
+++ src/main/java/org/jdriven/Starter.java
@@ -3,12 +3,10 @@ package org.jdriven;
 import org.jdriven.mapper.CarMapper;
 import org.jdriven.model.CarEntity;
 
-import static org.jdriven.MapperUtil.use;
-
 public class Starter {
     public static void main(String[] args) {
         var carEntity = new CarEntity(3, "BMW", "X2 M35i xDrive", 2025);
-        var car = use(CarMapper.class).map(carEntity);
+        var car = CarMapper.INSTANCE.map(carEntity);
 
         System.out.println(car.toString());
     }
--- src/main/java/org/jdriven/mapper/CarMapper.java
+++ src/main/java/org/jdriven/mapper/CarMapper.java
@@ -4,8 +4,12 @@ import org.jdriven.Mapper;
 import org.jdriven.model.Car;
 import org.jdriven.model.CarEntity;
 
+import static org.jdriven.MapperUtil.use;
+
 @Mapper
 public interface CarMapper {
+    CarMapper INSTANCE = use(CarMapper.class);
+
     Car map(CarEntity car);
     CarEntity map(Car car);
 }
